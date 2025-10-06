06b3168fa7eaa4da51ee62ee793513861bf990b0
FranciscoToty <franciscomascarello20@gmail.com>
2025-10-06 13:38:51 -0300

    feito

---
+++ firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDENLJSPpFeeZAqSGW4QYw1rG7wUDoDPnA",
+  authDomain: "bardof-c1b4d.firebaseapp.com",
+  databaseURL: "https://bardof-c1b4d-default-rtdb.firebaseio.com",
+  projectId: "bardof-c1b4d",
+  storageBucket: "bardof-c1b4d.firebasestorage.app",
+  messagingSenderId: "360363951793",
+  appId: "1:360363951793:web:3d707868438ee651fa0490"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
